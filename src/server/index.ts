7443d4b1f85c157ef773d0a6aa1f93fc4d659f5c
prabinpebam <prabinpebam@gmail.com>
2025-03-25 02:41:22 +0530

    Build deploy seems to be good, mongo DB connection issue - 10

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express';
 import path from 'path';
 import cors from 'cors';
-import { connectToDatabase } from '../database/config/db.config';
+import { connectToDatabase } from '../database/config/db.prod.config';
 import { addWorkflow, updateWorkflow, listWorkflows, getWorkflow, deleteWorkflow } from './controllers/workflow.controller';
 import { upload } from './middleware/upload.middleware';
 import { FileService } from './services/file.service';
